15d0af3ab5d56526730bf8ef1c593fbd851d7f0b
KmiloVm-code <kmilo7v@gmail.com>
2023-07-15 18:22:05 -0500

    experiment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,16 +6,6 @@ const Header = () => {
   return (
     <div className='flex p-10 justify-evenly'>
       <div className='grid content-evenly p-10 gap-7 w-1/2 mr-14'>
-        <div>
-          <p className='flex items-center gap-3'>
-            <span className='font-semibold'>Oferta</span>
-
-            <div className='flex px-4 py-1 items-center bg-black rounded-full gap-2'>
-              <BoltIcon className='w-5 h-5 text-primary-500' />
-              <span className=' text-white'> FULL</span>
-            </div>
-          </p>
-        </div>
 
         <div>
           <p className='text-4xl font-bold m-4'>
@@ -23,21 +13,6 @@ const Header = () => {
           </p>
         </div>
 
-        <div className='grid grid-flow-col justify-stretch gap-10'>
-          <div className='flex items-center'>
-            <div className='flex flex-col p-4 items-center bg-secondary rounded-full w-full h-20'>
-              <span className='font-semibold'>OFERTAS HASTA</span>
-              <span className='font-bold'>25% OFF</span>
-            </div>
-          </div>
-
-          <div className='flex items-center'>
-            <div className='flex flex-col p-4 items-center bg-secondary rounded-full w-full h-20'>
-              <span className='font-semibold'>ENVÍOS NACIONALES</span>
-              <img src={Shipping} alt="shipping" className='w-6 h-6' />
-            </div>
-          </div>
-        </div>
       </div>
 
       <div className='flex w-1/2'>
