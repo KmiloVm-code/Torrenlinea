efd514c95a4f41bf58fcc4ce94635475f7a889b4
KmiloVm-code <kmilo7v@gmail.com>
2023-07-17 20:44:12 -0500

    Add react-router-dom and configure the routes for shop

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,12 +1,28 @@
 import Home from '../Home'
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import Menu from '../../components/Menu';
+import Shop from '../Shop';
 
 function App() {
 
+  const AppRoutes = () => {
+    let routes = useRoutes([
+      { path: '/', element: <Home /> },
+      { path: '/tienda', element: <Shop /> },
+    ]);
+    return routes;
+  };
+
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Menu />
+      <AppRoutes />
+      <Footer />
+    </BrowserRouter>
   )
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,16 @@
 import Brands from '../../components/Brands'
 import Categories from '../../components/Categories'
-import Footer from '../../components/Footer'
 import Header from '../../components/Header'
-import Menu from '../../components/Menu'
 import MostSelledProducts from '../../components/MostSelledProducts'
-import Navbar from '../../components/Navbar'
+
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <Menu />
       <Header />
       <MostSelledProducts />
       <Categories />
       <Brands />
-      <Footer />
     </>
   )
 }
