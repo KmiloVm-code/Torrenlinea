b81326b5ab4a02cb19d0a515432d261675c47257
KmiloVm-code <kmilo7v@gmail.com>
2023-09-10 22:25:21 -0500

    fix menu on mobile devices

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,28 +4,26 @@ import { NavbarContext } from "../../contexts/NavbarContext";
 import { useFetchCategories } from "../../api/useFetchCategories";
 
 const Menu = () => {
-
-  const navBarContext = useContext(NavbarContext);
-
+  const { isMenuOpen, toggleMenu } = useContext(NavbarContext);
   const { categories, isLoading, error } = useFetchCategories();
 
   return (
-    <div className={`${navBarContext.isMenuOpen ? 'flex' : 'hidden'} md:flex flex-col h-screen md:h-auto p-4 md:pl-14 bg-gray-900`}>
-      <ul className={`flex flex-col md:flex-row md:items-center md:justify-start gap-5 mb-5 text-lg text-white font-medium`}>
-        <li>
+    <div className={`flex ${isMenuOpen ? '' : 'hidden'} md:flex flex-col h-screen md:h-auto p-4 md:pl-14 bg-gray-900`}>
+      <ul className="flex flex-col md:flex-row md:items-center md:justify-start gap-5 mb-5 text-lg text-white font-medium">
+        <li onClick={toggleMenu}>
           <NavLink to='/'>
             Inicio
           </NavLink>
         </li>
         <li>
-          <NavLink to='/tienda'>
+          <NavLink to='/tienda' onClick={toggleMenu}>
             Todos los productos
           </NavLink>
         </li>
         {isLoading && <p>Loading...</p>}
         {error && <p>Something went wrong</p>}
         {categories?.filter(category => category.name !== 'Uncategorized').map(category => (
-          <li key={category.id}>
+          <li key={category.id} onClick={toggleMenu}>
             <NavLink to={`/tienda/${category.slug}`}>
               {category.name}
             </NavLink>
@@ -34,10 +32,10 @@ const Menu = () => {
       </ul>
 
       <ul className="flex flex-col md:hidden gap-5 pt-8 text-lg text-white font-medium border-t border-t-white">
-        <li>
+        <li onClick={toggleMenu}>
           Mis compras
         </li>
-        <li>
+        <li onClick={toggleMenu}>
           Iniciar sesion
         </li>
       </ul>
@@ -45,4 +43,4 @@ const Menu = () => {
   )
 }
 
-export default Menu
+export default Menu;
