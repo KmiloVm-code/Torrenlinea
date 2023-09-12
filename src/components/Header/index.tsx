6ee4a5ea64efbf9a8b6c62dd6c58edc3dff07491
KmiloVm-code <kmilo7v@gmail.com>
2023-09-11 20:08:33 -0500

    redesign of the Header offer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,30 @@
-import Offer from '../../assets/offer.png'
+import Offer from '../../assets/offer.png';
 
 const Header = () => {
   return (
     <div className='grid sm:grid-cols-2 p-4 md:p-16 justify-center items-center md:justify-between mb-5 md:mb-10 bg-black/80'>
 
       <div className='md:mr-4'>
-        <div className='flex flex-col gap-4 sm:gap-10 text-white'>
-          <h1 className='text-2xl md:text-4xl font-bold'>
+        <div className='flex flex-col gap-6 sm:gap-10 text-white'>
+          <h1 className='text-2xl md:text-4xl text-center sm:text-start font-bold mt-12 sm:mt-0'>
             Descubre una nueva dimensión de sonido con los AirPods 3 Pro
           </h1>
-          <p className='text-base md:text-lg font-medium'>
+          <p className='text-base md:text-lg font-medium text-center sm:text-start mx-10 sm:mx-0 mb-5'>
             La mejor experiencia de sonido con la mejor calidad de audio
           </p>
-          <p className='p-2 md:p-3 text-base md:text-lg font-medium rounded-lg border border-primary-500 shadow-md shadow-primary-400 w-fit'>
+          <p className='flex justify-center p-2 md:p-3 text-base md:text-lg font-medium rounded-lg border border-primary-500 shadow-md shadow-primary-400 w-full sm:w-fit cursor-pointer'>
             ¡Adquiérelos hoy mismo!
           </p>
         </div>
       </div>
 
-      <div className='flex justify-center items-center mt-4 md:mt-0'>
+      <div className='flex justify-center items-center my-10 sm:my-0 sm:mt-4 md:mt-0'>
         <figure>
           <img src={Offer} alt="offer" className='object-contain rounded-lg' />
         </figure>
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Header
+export default Header;
